62d4cdb3dea98973fc63e43ea32fbe6ede89f9c5
thegoatmufasa <tserebour1@gmail.com>
2024-05-24 12:25:06 +0000

    3

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,15 +1,24 @@
 use std::net::TcpStream;
+use std::io::{Read, Write};
+// use std::io::Result;
 
 fn main(){
-    let stream = TcpStream::connect("127.0.0.1:8080");
+    let mut stream = TcpStream::connect("127.0.0.1:8080").expect("connect");
 
-    match stream{
-        Ok(stream) => {
-            println!("Connected");
-        },
-        Err(e) => {
-            println!("Error: {}", e);
-        }
-    }
+    let mut buffer = [0; 1024];
+
+    let request = String::from_utf8_lossy(&buffer[..]);
+    println!("Read {} bytes", request);
+    
+
+    let a = stream.read(&mut buffer);
+    println!("{:?}",a);
+
+    
+    
+
+    
+
+   
 }
 
